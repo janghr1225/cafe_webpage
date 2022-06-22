b56cac04630bc7bb75023fe91f1dd5963f7d6aa4
Pyotato <pyolovely01@gmail.com>
2022-06-22 14:54:24 +0900

    make js file and add menu_FOOD and _DRINKS main.js's

---
+++ js/menu_DRINKS_main.js
@@ -0,0 +1,125 @@
+//json으로 음료 분류
+let drinkMenu = {
+  data: [
+    {
+      //1
+      name: "프리미엄 블랜드 아메리카노", //이름
+      type: "커피", //분류
+      engname: "Premium Blend Americano", //영문이름
+      calories: 13, //칼로리
+      sugar: 0, //당류
+      protein: 1, //단백질
+      transfat: 0.1, //포화지방
+      sodium: 4, //나트륨
+      caffeine: 233, //카페인
+      //설명
+      description:
+        "화사한 산미와 과일과 같은 달콤함, 깔끔한 후미를 즐길수 있는 커피",
+      //이미지 태그
+      img: '<img src="./img/menu_Drinks/1_premiumBlendAmericano.png">',
+    },
+    {
+      //2
+      name: "카페라떼",
+      type: "커피",
+      engname: "Caffe Latte",
+      calories: 180,
+      sugar: 13,
+      protein: 8,
+      transfat: 6,
+      sodium: 130,
+      caffeine: 127,
+      description:
+        "진한 에스프레소에 우유를 넣어 풍부한 커피향을 부드럽게 즐길 수 있는 커피",
+      img: '<img src="./img/menu_Drinks/2_cafeLatte.png">',
+    },
+    {
+      //3
+      name: "콜드브루 라떼", //이름
+      type: "커피", //분류
+      engname: "Cold Brew Latte", //영문이름
+      calories: 125, //칼로리
+      sugar: 9, //당류
+      protein: 5, //단백질
+      transfat: 3.9, //포화지방
+      sodium: 85, //나트륨
+      caffeine: 148, //카페인
+      //설명
+      description: "콜드브루 커피의 풍부한 풍미와 우유의 고소함이 더해진 라떼",
+      //이미지 태그
+      img: '<img src="./img/menu_Drinks/3_coldBrewLatte.png">',
+    },
+    {
+      //3
+      name: "콜드브루 라떼", //이름
+      type: "커피", //분류
+      engname: "Cold Brew Latte", //영문이름
+      calories: 125, //칼로리
+      sugar: 9, //당류
+      protein: 5, //단백질
+      transfat: 3.9, //포화지방
+      sodium: 85, //나트륨
+      caffeine: 148, //카페인
+      //설명
+      description: "콜드브루 커피의 풍부한 풍미와 우유의 고소함이 더해진 라떼",
+      //이미지 태그
+      img: '<img src="./img/menu_Drinks/3_coldBrewLatte.png">',
+    },
+    {
+      //4
+      name: "디카페인 바닐라 딜라이트", //이름
+      type: "커피", //분류
+      engname: "Vanilla Delight (Decaf blend)", //영문이름
+      calories: 202, //칼로리
+      sugar: 24, //당류
+      protein: 6, //단백질
+      transfat: 6.1, //포화지방
+      sodium: 161, //나트륨
+      caffeine: 20, //카페인
+      //설명
+      description: "콜드브루 커피의 풍부한 풍미와 우유의 고소함이 더해진 라떼",
+      //이미지 태그
+      img: '<img src="./img/menu_Drinks/4_decafvanilladelight.png">',
+    },
+    {
+      //5
+      name: "카페모카", //이름
+      type: "커피", //분류
+      engname: "Caffe Mocha", //영문이름
+      calories: 251, //칼로리
+      sugar: 28, //당류
+      protein: 8, //단백질
+      transfat: 5.9, //포화지방
+      sodium: 148, //나트륨
+      caffeine: 132, //카페인
+      //설명
+      description:
+        "진한 에스프레소와 우유, 달콤 쌉싸름한 초콜릿이 어우러진 커피",
+      //이미지 태그
+      img: '<img src="./img/menu_Drinks/5_cafeMocha.png">',
+    },
+    {
+      //6
+      name: "카페모카", //이름
+      type: "커피", //분류
+      engname: "Caffe Mocha", //영문이름
+      calories: 251, //칼로리
+      sugar: 28, //당류
+      protein: 8, //단백질
+      transfat: 5.9, //포화지방
+      sodium: 148, //나트륨
+      caffeine: 132, //카페인
+      //설명
+      description:
+        "진한 에스프레소와 우유, 달콤 쌉싸름한 초콜릿이 어우러진 커피",
+      //이미지 태그
+      img: '<img src="./img/menu_Drinks/5_cafeMocha.png">',
+    },
+  ], //
+  /*      totalCount: 861,
+      totalPages: 87,
+      currentPage: 1,
+      pageSize: 10,
+      fromDate: null,
+      toDate: null,*/
+};
